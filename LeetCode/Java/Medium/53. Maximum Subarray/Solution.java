f2665f922fcf18b805521aa01b4788f29d220bad
Yatharth Singh <yatharthsingh74644@gmail.com>
2026-09-24 16:48:17 +0530

    Add Medium Java Solution for 53. Maximum Subarray from LeetCode

--- LeetCode/Java/Medium/53. Maximum Subarray/Solution.java
+++ LeetCode/Java/Medium/53. Maximum Subarray/Solution.java
@@ -2,14 +2,15 @@ class Solution {
     public int maxSubArray(int[] nums) {
         int ans=Integer.MIN_VALUE;
         int n=nums.length;
-        for(int i=0;i<n;i++)
+        int sum=0;
+        for(int val:nums)
         {
-            int sum=0;
-            for (int j=i;j<n;j++){
-                sum= sum+nums[j];
-                ans= Math.max(ans,sum);
+            sum+=val;
+            ans=Math.max(ans,sum);
+            if(sum<0){
+                sum=0;
             }
-        }
+            }
         return ans;
     }
 }
